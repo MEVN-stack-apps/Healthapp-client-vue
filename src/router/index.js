86bca5fc57d44408ce8f879f1a813cf8edb9bc2a
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2022-12-20 16:15:49 +1100

    add auth guard to routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,14 @@ import LoginView from '../views/LoginView.vue';
 import RegisterView from '../views/RegisterView.vue';
 import store from '@/store/index';
 
+function authGuard(to, from, next) {
+  if (!store.getters.token) {
+    next({ name: 'Login' });
+  } else {
+    next();
+  }
+}
+
 const routes = [
   {
     path: '/',
@@ -20,19 +28,13 @@ const routes = [
     component: RegisterView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/profile',
+    name: 'profile',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-    beforeEnter(to, from, next) {
-      if (!store.getters.token) {
-        next({ name: 'Login' });
-      } else {
-        next();
-      }
-    }
+    component: () => import(/* webpackChunkName: "about" */ '../views/ProfileView.vue'),
+    beforeEnter: authGuard
   },
 
 ];
