e453061e75ac7d7ea482f7ad2feb6d21e8ebf619
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2022-12-20 16:16:44 +1100

    store set up for user authentication

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,62 @@
 import { createStore } from 'vuex';
-import { postJson } from '@/utils/https';
+import { postJson, } from '@/utils/https';
 
 export default createStore({
   state: {
-    token: localStorage.getItem('auth-token')
+    token: localStorage.getItem('auth-token'),
+    // profile: {
+    //   firstName: '',
+    //   lastName: '',
+    //   gender: '',
+    //   yearOfBirth: ''
+    // },
   },
   getters: {
     token(state) {
       return state.token;
-    }
+    },
+    // profile(state) {
+    //   return state.profile;
+    // },
   },
   mutations: {
     setToken(state, value) {
-      state.token = value;
-      localStorage.setItem('auth-token', value);
-    }
+      if (value) {
+        state.token = value;
+        localStorage.setItem('auth-token', value);
+      } else {
+        state.token = null;
+        localStorage.removeItem('auth-token');
+      }
+    },
+    // setProfile(state, profile) {
+    //   state.profile = profile;
+    // },
   },
   actions: {
+    // addProfile(context, data) {
+    //   return postJson({
+    //     url: '/profile',
+    //     data
+    //   }).then(data => {
+    //     console.log("profile in action:", data);
+    //     if (data.profile) {
+    //       context.commit('setProfile', data.profile);
+    //       return data;
+    //     }
+    //   });
+    // },
+    // getProfile(context, user) {
+    //   return getJson({
+    //     url: `/profile/${user._id}`
+    //   }).then(data => {
+    //     if (data.err) {
+    //       return data;
+    //     }
+    //     context.commit('setProfile', data.profile);
+    //     return data;
+    //   });
+    // },
     registerUser(context, data) {
       return postJson({
         url: '/register',
