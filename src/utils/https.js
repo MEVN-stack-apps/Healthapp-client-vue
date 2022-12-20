1906fafa72f15454bed31126be12eced5e947521
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2022-12-20 16:18:04 +1100

    add post and get json api calling function

--- src/utils/https.js
+++ src/utils/https.js
@@ -1,26 +1,49 @@
-const baseURL = "http://localhost:3000";
+import router from '@/router/index';
+const baseURL = 'http://localhost:3000';
+
+function getHeaders() {
+  const headers = new Headers();
+  const token = localStorage.getItem('auth-token');
+  headers.append('Content-Type', 'application/json');
+
+  if (token) {
+    headers.append('Authorization', `Bearer ${token}`);
+  }
+  return headers;
+}
+
+function checkAuth(res) {
+  if (res.status === 401) {
+    router.push('/login');
+  }
+  return res.json();
+}
 
 const postJson = async function (options) {
-  const headers = new Headers();
-
-  headers.append('Content-Type', 'application/json');
+  const headers = getHeaders();
 
   const response = await fetch(`${baseURL}${options.url}`, {
     method: 'post',
     headers,
-    body: JSON.stringify(options.data)
-  }).then(res => {
-    const data = res.json();
-    return data;
-  }).then(obj => {
-    if (obj.token) {
-      localStorage.setItem('auth-token', obj.token);
-    }
-    return obj;
-  });
+    body: JSON.stringify(options.data),
+  }).then(checkAuth);
+
   return response;
 };
 
-module.exports = {
-  postJson
+const getJson = async function (options) {
+  const headers = getHeaders();
+
+  const response = await fetch(`${baseURL}${options.url}`, {
+    method: 'get',
+    headers,
+  }).then(checkAuth);
+
+  return response;
+};
+
+
+export {
+  postJson,
+  getJson,
 };
